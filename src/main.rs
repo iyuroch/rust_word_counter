d0000468537ee6cf78dbcd07f27ea49651db67c7
iyuroch <iyuroch@gmail.com>
2018-06-13 10:20:22 +0300

    Borrowed value error

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::sync::mpsc::channel;
-use std::thread;
-use std::fs::File;
-use std::io::Read;
-
+extern crate spmc;
 extern crate serde;
 extern crate serde_json;
+
+use std::fs::File;
+use std::thread;
+use std::string;
+use std::io::{self, Read};
 use serde_json::{Value, Error};
 
 #[macro_use]
@@ -27,15 +28,54 @@ fn read_config() -> Config {
     return config;
 }
 
+fn filename_to_string(s: &str) -> io::Result<String> {
+    let mut file = File::open(s)?;
+    let mut s = String::new();
+    file.read_to_string(&mut s)?
+    Ok(s)
+}
+
+fn words_by_line<'a>(s: &'a str) -> Vec<&'a str> {
+    let chars: &[_] = &[',', '"', '.', '!', '!', '(', ')'];
+    return s.split_whitespace()
+        .map(|x| x.trim_matches(chars))//trim_matches(chars))
+        .collect()
+}
+
 fn main() {
     let config: Config = read_config();
-    let (sender, receiver)
-    //let (sender, receiver) = channel();
-    //thread::spawn(move|| {
-    //    sender.send("Hello!");
-    //});
-    //// just something here
-    //println!("{:?}", receiver.recv().unwrap());
-    //println!("Hello, world!");
+
+    let (tx, rx) = spmc::channel();
+    let mut thread_pool = Vec::new();
+
+    for n in 0..5 {
+        let rx = rx.clone();
+        thread_pool.push(thread::spawn(move || {
+            let words = rx.recv().unwrap();
+
+            //let mut frequency: HashMap<&str, u32> = HashMap::new();
+            //for word in &words { // word is a &str
+            //    *frequency.entry(word.to_lowercase()).or_insert(0) += 1;
+            //}
+            //println!("{:?}", frequency);
+            println!("worker {} recvd: {:?}", n, words);
+        }));
+    }
+
+    let whole_file = filename_to_string(&config.read_file).unwrap();
+    let words = words_by_line(&whole_file);
+
+    for chunk in words.chunks(100) {
+        tx.send("Hi").unwrap();
+        //tx.send(chunk.to_owned()).unwrap();
+        println!("len: {}", chunk.len());
+        //println!("{:?}", chunk);
+    }
+    drop(tx);
+
+    for thread_ in thread_pool {
+      thread_.join().unwrap();
+    }
+
     println!("{}", config.threads);
 }
