d06018def6f7e153d3f8aab74e4a85cc819f58eb
iyuroch <iyuroch@gmail.com>
2018-07-12 20:18:45 +0300

    Changed arc to boxes, this fixed problems with deadlock

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ use std::sync::atomic::{AtomicUsize, Ordering, ATOMIC_USIZE_INIT};
 use std::collections::VecDeque;
 use std::str;
 //TODO: add condvar to wake up threads
+//TODO: remove dropping - we can lose some data in that case
 //TODO: we don't need the sender - as we operate with pointers and lock anyway
 
 #[derive(Serialize, Deserialize)]
@@ -37,19 +38,19 @@ fn read_config() -> Config {
     return config;
 }
 
-fn read_file_char(s: &str, tx: spmc::Sender<Arc<Mutex<Vec<Arc<Vec<u8>>>>>>) -> std::io::Result<()> {
+fn read_file_char(s: &str, tx: spmc::Sender<Box<Vec<Box<Vec<u8>>>>>) -> std::io::Result<()> {
     let f = BufReader::new(File::open(s)?);
     let mut _words_counter = 0;
-    let mut words_vec = Arc::new(Mutex::new(vec![]));
+    let mut words_vec = Box::new(vec![]);
 
     for ch_vec in f.split(b' ') {
-        let word = Arc::new(ch_vec.unwrap().to_owned());
+        let word = Box::new(ch_vec.unwrap().to_owned());
          
-        if words_vec.lock().unwrap().len() == 100 {
+        if words_vec.len() == 100 {
             tx.send(words_vec.clone()).expect("Cannot send word to channel");
-            words_vec = Arc::new(Mutex::new(vec![]));
+            words_vec = Box::new(vec![]);
         }
-        words_vec.lock().unwrap().push(word.clone());
+        words_vec.push(word.clone());
     }
 
     tx.send(words_vec.clone()).expect("Cannot send word to channel");
@@ -57,29 +58,30 @@ fn read_file_char(s: &str, tx: spmc::Sender<Arc<Mutex<Vec<Arc<Vec<u8>>>>>>) -> s
     Ok(())
 }
 
-fn count_words(vec_rx: spmc::Receiver<Arc<Mutex<Vec<Arc<Vec<u8>>>>>>, dict_queue: Arc<Mutex<VecDeque<Arc<Mutex<HashMap<Arc<Vec<u8>>,u32>>>>>>) {
+fn count_words(vec_rx: spmc::Receiver<Box<Vec<Box<Vec<u8>>>>>, dict_queue: Arc<Mutex<VecDeque<Box<HashMap<String,u32>>>>>) {
     //let _ignore_chars: Vec<String> = vec!["\'", "\"", ",", ".", ";"].iter().map(|&s| s.into()).collect();
-    let mut occurrences = Arc::new(Mutex::new(HashMap::new()));
+    let mut occurrences = Box::new(HashMap::new());
     'outer: loop {
         let words = vec_rx.recv();
         match words {
-            Ok(value) => {
-                for word in value.lock().unwrap().iter() {
-                    *occurrences.lock().unwrap().entry(word.clone()).or_insert(0) += 1;
+            Ok(mut value) => {
+                for char_vec in value.iter_mut() {
+                    let word = String::from(str::from_utf8(&char_vec).unwrap());
+                    *occurrences.entry(word).or_insert(0) += 1;
                 }
             }
             Err(spmc::RecvError) => break 'outer,
         }
     }
-    dict_queue.lock().unwrap().push_back(occurrences.clone());
+    dict_queue.lock().unwrap().push_back(occurrences);
     VEC_COUNT_JOBS.fetch_sub(1, Ordering::SeqCst);
 }
 
-fn collect_dict(dict_queue: Arc<Mutex<VecDeque<Arc<Mutex<HashMap<Arc<Vec<u8>>,u32>>>>>>) {
+fn collect_dict(dict_queue: Arc<Mutex<VecDeque<Box<HashMap<String,u32>>>>>) {
     //TODO: add cond var to wak them up as needed
     //TODO: test which map is bigger
-    let mut first_map = Arc::new(Mutex::new(HashMap::new()));
-    let mut second_map = Arc::new(Mutex::new(HashMap::new()));
+    let mut first_map;
+    let mut second_map;
     'outer: loop {
         {
             let mut queue = dict_queue.lock().unwrap();
@@ -88,33 +90,29 @@ fn collect_dict(dict_queue: Arc<Mutex<VecDeque<Arc<Mutex<HashMap<Arc<Vec<u8>>,u3
             } else {
                 //check for condvar wake up
                 //test if enough length - if not continue outer
-                println!("NOO");
                 if queue.len() > 1 {
-                    first_map = Arc::clone(&queue.pop_front().unwrap());
-                    second_map = Arc::clone(&queue.pop_front().unwrap());
+                    first_map = queue.pop_front().unwrap();
+                    second_map = queue.pop_front().unwrap();
                     drop(queue);
-                    for (key, value) in first_map.lock().unwrap().iter_mut() {
-                        let mut prim_map = second_map.lock().unwrap(); 
-                        let mut counter = *prim_map.entry(key.clone()).or_insert(*value);
-                        counter += *value;
+                    for (key, value) in first_map.iter_mut() {
+                        *second_map.entry(key.to_string()).or_insert(*value) += 1;
+                        continue;
                     }
+                    dict_queue.lock().unwrap().push_back(second_map);
                 }
             }
         }
-        dict_queue.lock().unwrap().push_back(second_map.clone());
     }
     return;
 }
 
 fn main() {
-    let (vec_tx, vec_rx) = spmc::channel::<Arc<Mutex<Vec<Arc<Vec<u8>>>>>>();
+    let (vec_tx, vec_rx) = spmc::channel::<Box<Vec<Box<Vec<u8>>>>>();
     let dict_queue = Arc::new(Mutex::new(VecDeque::new()));
     let thread_pool = rayon::ThreadPoolBuilder::new().num_threads(3).build().unwrap();
     let config: Config = read_config();
 
-    let words_vec_alarm = Condvar::new();
-
-    for _ in 0..2 {
+    for _ in 0..3 {
         let vec_rx = vec_rx.clone();
         let dict_queue = dict_queue.clone();
         VEC_COUNT_JOBS.fetch_add(1, Ordering::SeqCst);
@@ -126,13 +124,15 @@ fn main() {
    
     for _ in 0..1 {
         let dict_queue = dict_queue.clone();
-        let n = thread_pool.install(move || collect_dict(dict_queue));
+        let _n = thread_pool.install(move || collect_dict(dict_queue));
     }
 
-    let first_map = Arc::clone(&dict_queue.lock().unwrap().pop_front().unwrap());
+    println!("{}", dict_queue.lock().unwrap().len());
 
-    for (key, value) in first_map.lock().unwrap().iter_mut() {
-        let sparkle_heart = str::from_utf8(&key).unwrap();
- //       println!("{}, {}", sparkle_heart, value);
+    for item in dict_queue.lock().unwrap().iter() {
+        // println!("{:?}", item);
+        println!("{:?}", item.keys().len());
+        continue
     }
+    // we have to pop front one element after everything finished
 }
