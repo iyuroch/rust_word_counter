d94f90f51955208e0a54de509e1beebee4039fae
iyuroch <iyuroch@gmail.com>
2018-07-14 00:54:18 +0300

    Added wake-up channel

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,23 @@ extern crate spmc;
 extern crate serde;
 extern crate serde_json;
 extern crate rayon;
+extern crate crossbeam;
+extern crate crossbeam_channel;
 
 #[macro_use]
 extern crate serde_derive;
 
 use std::io::BufRead;
-use std::sync::{Arc, Mutex, Condvar};
+use std::sync::{Arc, Mutex};
 use std::fs::File;
 use std::io::{Read, BufReader};
 use std::collections::HashMap;
-use std::sync::atomic::{AtomicUsize, Ordering, ATOMIC_USIZE_INIT};
 use std::collections::VecDeque;
 use std::str;
-//TODO: add condvar to wake up threads
-//TODO: remove dropping - we can lose some data in that case
-//TODO: we don't need the sender - as we operate with pointers and lock anyway
+
+//TODO: add exception handling for the file read
+//TODO: no fastest way to clear string from chars - need to fix this
+//TODO: test which map is bigger and iterate over smaller - will save time
 
 #[derive(Serialize, Deserialize)]
 struct Config {
@@ -27,18 +29,17 @@ struct Config {
     nume_count: String,
 }
 
-static VEC_COUNT_JOBS: AtomicUsize = ATOMIC_USIZE_INIT;
-
 fn read_config() -> Config {
-    // TODO: add exception handling for the file read
     let mut file = File::open("config.json").unwrap();
     let mut data = String::new();
     file.read_to_string(&mut data).unwrap();
-    let config: Config = serde_json::from_str(&mut data).expect("Couldn't read config");
+    let config: Config = serde_json::from_str(&mut data)
+                            .expect("Couldn't read config");
     return config;
 }
 
-fn read_file_char(s: &str, tx: spmc::Sender<Box<Vec<Box<Vec<u8>>>>>) -> std::io::Result<()> {
+fn read_file_char(s: &str, tx: spmc::Sender<Box<Vec<Box<Vec<u8>>>>>) 
+                -> std::io::Result<()> {
     let f = BufReader::new(File::open(s)?);
     let mut _words_counter = 0;
     let mut words_vec = Box::new(vec![]);
@@ -58,81 +59,110 @@ fn read_file_char(s: &str, tx: spmc::Sender<Box<Vec<Box<Vec<u8>>>>>) -> std::io:
     Ok(())
 }
 
-fn count_words(vec_rx: spmc::Receiver<Box<Vec<Box<Vec<u8>>>>>, dict_queue: Arc<Mutex<VecDeque<Box<HashMap<String,u32>>>>>) {
-    //let _ignore_chars: Vec<String> = vec!["\'", "\"", ",", ".", ";"].iter().map(|&s| s.into()).collect();
+fn count_words(vec_rx: spmc::Receiver<Box<Vec<Box<Vec<u8>>>>>, 
+                dict_queue: Arc<Mutex<VecDeque<Box<HashMap<String,u32>>>>>,
+                wake_tx: crossbeam_channel::Sender<bool>) {
     let mut occurrences = Box::new(HashMap::new());
     'outer: loop {
         let words = vec_rx.recv();
         match words {
             Ok(mut value) => {
                 for char_vec in value.iter_mut() {
-                    let word = String::from(str::from_utf8(&char_vec).unwrap());
-                    *occurrences.entry(word).or_insert(0) += 1;
+                    // we need to take string, split by \n, trim all chars and lowercase it
+                    // not the fastest - but we use std for the rescue
+                    // also not sure if split by \n here
+                    let mut raw_words = String::from(str::from_utf8(&char_vec).unwrap());
+                    for raw_word in raw_words.split("\n") {
+                        let word = raw_word.trim_matches(|c| c == ',' || c == '.' 
+                                        || c == ';' || c == ' ').to_lowercase();
+                        *occurrences.entry(word).or_insert(0) += 1;
+                    }
+
                 }
             }
             Err(spmc::RecvError) => break 'outer,
         }
     }
     dict_queue.lock().unwrap().push_back(occurrences);
-    VEC_COUNT_JOBS.fetch_sub(1, Ordering::SeqCst);
+    //we notify our collect dictionary that we pushed and close channel after
+    wake_tx.send(true);
+    drop(wake_tx);
 }
 
-fn collect_dict(dict_queue: Arc<Mutex<VecDeque<Box<HashMap<String,u32>>>>>) {
-    //TODO: add cond var to wak them up as needed
-    //TODO: test which map is bigger
+fn collect_dict(dict_queue: Arc<Mutex<VecDeque<Box<HashMap<String,u32>>>>>,
+                wake_rx: crossbeam_channel::Receiver<bool>) {
     let mut first_map;
     let mut second_map;
+    let mut finish_reading = false;
     'outer: loop {
         {
+            //our recv is blocking operation - it will wait for recv
+            //if all sender closed - return none
+            wake_rx.recv();
+            match wake_rx.recv() {
+                Some(v) => println!("{}", v),
+                None => finish_reading = true,
+            };
+            // we need to check before starting merging - if no words counter
+            // and length 1 - we can stop merging and break outer
             let mut queue = dict_queue.lock().unwrap();
-            if queue.len() < 2 && VEC_COUNT_JOBS.load(Ordering::SeqCst) == 0 {
-                break 'outer
-            } else {
-                //check for condvar wake up
-                //test if enough length - if not continue outer
-                if queue.len() > 1 {
-                    first_map = queue.pop_front().unwrap();
-                    second_map = queue.pop_front().unwrap();
-                    drop(queue);
-                    for (key, value) in first_map.iter_mut() {
-                        *second_map.entry(key.to_string()).or_insert(*value) += 1;
-                        continue;
-                    }
-                    dict_queue.lock().unwrap().push_back(second_map);
+            if queue.len() > 1 {
+                first_map = queue.pop_front().unwrap();
+                second_map = queue.pop_front().unwrap();
+                drop(queue);
+                for (key, value) in first_map.iter_mut() {
+                    *second_map.entry(key.to_string()).or_insert(*value) += 1;
+                    continue;
                 }
+                dict_queue.lock().unwrap().push_back(second_map);
+            } else if queue.len() == 1 && finish_reading {
+                break 'outer;
             }
         }
     }
-    return;
 }
 
 fn main() {
     let (vec_tx, vec_rx) = spmc::channel::<Box<Vec<Box<Vec<u8>>>>>();
+    let (wake_tx, wake_rx) = crossbeam_channel::unbounded();
+
     let dict_queue = Arc::new(Mutex::new(VecDeque::new()));
-    let thread_pool = rayon::ThreadPoolBuilder::new().num_threads(3).build().unwrap();
+    let thread_pool = rayon::ThreadPoolBuilder::new().num_threads(3)
+                        .build().unwrap();
+
     let config: Config = read_config();
 
-    for _ in 0..3 {
+    for _ in 0..1 {
         let vec_rx = vec_rx.clone();
+        let wake_tx = wake_tx.clone();
         let dict_queue = dict_queue.clone();
-        VEC_COUNT_JOBS.fetch_add(1, Ordering::SeqCst);
-        thread_pool.spawn(move || count_words(vec_rx, dict_queue));
+
+        thread_pool.spawn(move || count_words(vec_rx, dict_queue, wake_tx));
     }
 
-    read_file_char(&config.read_file, vec_tx).expect("Cannot read file char by char");
+    //we have extra copy of sender here - need to close it aswell
+    drop(wake_tx);
 
+    read_file_char(&config.read_file, vec_tx)
+        .expect("Cannot read file char by char");
    
     for _ in 0..1 {
         let dict_queue = dict_queue.clone();
-        let _n = thread_pool.install(move || collect_dict(dict_queue));
+        let wake_rx = wake_rx.clone();
+
+        let _n = thread_pool.install(move || collect_dict(dict_queue, wake_rx));
     }
 
-    println!("{}", dict_queue.lock().unwrap().len());
-
-    for item in dict_queue.lock().unwrap().iter() {
-        // println!("{:?}", item);
-        println!("{:?}", item.keys().len());
-        continue
+    let res_dict = dict_queue.lock().unwrap().pop_front();
+    match res_dict {
+        Some(v) => {
+            let mut count_vec: Vec<(&String, &u32)> = v.iter().collect();
+            count_vec.sort_by(|a, b| b.1.cmp(a.1));
+            println!("{:?}", count_vec);
+            count_vec.sort_by(|a, b| a.0.cmp(b.0));
+            println!("{:?}", count_vec);
+        },
+        None => (),
     }
-    // we have to pop front one element after everything finished
+    
 }
